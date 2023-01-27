0039a25dc475926dea4c9578254fea88320dc380
yeowool1010 <qyoong3579@gmail.com>
2023-01-27 21:32:02 +0900

    💄 [ui] 메인,페이지네이션,검색박스 클래스 이름 변경

--- old_shopping_mall/src/components/SearchBox.js
+++ old_shopping_mall/src/components/SearchBox.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/SearchBox.css";
 
 function SearchBox() {
   return (
     <section id="search-box">
-      <h2 class="">검색</h2>
-      <div class="search-inner">
+      <h2>검색</h2>
+      <div className="search-inner">
         <select id="select-box">
           <option id="selectOption">전체</option>
           <option id="selectOption">상품명</option>
           <option id="selectOption">브렌드</option>
           <option id="selectOption">상품내용</option>
         </select>
-        <input class="input-box" type="text" placeholder="입력하세요."></input>
-        <button class="search-btn">조회</button>
+        <input
+          className="input-box"
+          type="text"
+          placeholder="입력하세요."
+        ></input>
+        <button className="search-btn">조회</button>
       </div>
     </section>
   );
