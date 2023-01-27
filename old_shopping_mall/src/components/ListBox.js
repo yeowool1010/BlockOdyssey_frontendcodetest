6c7f7c0145fd1b7888224d6c926e56aec33f876c
yeowool1010 <qyoong3579@gmail.com>
2023-01-27 14:39:52 +0900

    💄 [ui] 상품 리스트 퍼블리싱 완료 #1

--- old_shopping_mall/src/components/ListBox.js
+++ old_shopping_mall/src/components/ListBox.js
@@ -1,39 +1,61 @@
 import React from "react";
+import "../styles/ListBox.css";
 
 function ListBox() {
   return (
-    <article id="list-box">
-      <hgroup id="list-box-header">
-        <p>상품번호</p>
-        <p>상품평</p>
-        <p>브랜드</p>
-        <p>상품내용</p>
-        <p>가격</p>
-        <p>평점</p>
-        <p>재고</p>
-      </hgroup>
-
-      <section id="list-box-innder">
-        <p>1</p>
-        <p>상품번호</p>
-        <p>상품평</p>
-        <p>브랜드</p>
-        <p>상품내용</p>
-        <p>가격</p>
-        <p>평점</p>
-        <p>재고</p>
-      </section>
-      <section id="list-box-innder">
-        <p>1</p>
-        <p>상품번호</p>
-        <p>상품평</p>
-        <p>브랜드</p>
-        <p>상품내용</p>
-        <p>가격</p>
-        <p>평점</p>
-        <p>재고</p>
-      </section>
-    </article>
+    <section id="list-box">
+      <ul>
+        <li>상품리스트</li>
+        <li>
+          <ul id="list-table">
+            <li>
+              <ul>
+                <li>상품번호</li>
+                <li>상품명</li>
+                <li>브랜드</li>
+                <li>상품내용</li>
+                <li>가격</li>
+                <li>평점</li>
+                <li>재고</li>
+              </ul>
+            </li>
+            <li>
+              <ul>
+                <li>1</li>
+                <li>아이폰</li>
+                <li>애플</li>
+                <li>내용네요ㅕㅇ이.이다아아아</li>
+                <li>1000</li>
+                <li>4.4</li>
+                <li>94</li>
+              </ul>
+            </li>
+            <li>
+              <ul>
+                <li>1</li>
+                <li>아이폰</li>
+                <li>애플</li>
+                <li>내용네요ㅕㅇ이.이다아아아</li>
+                <li>1000</li>
+                <li>4.4</li>
+                <li>94</li>
+              </ul>
+            </li>
+            <li>
+              <ul>
+                <li>1</li>
+                <li>아이폰</li>
+                <li>애플</li>
+                <li>내용네요ㅕㅇ이.이다아아아</li>
+                <li>1000</li>
+                <li>4.4</li>
+                <li>94</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </section>
   );
 }
 
