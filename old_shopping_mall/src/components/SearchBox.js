69d3a1d3117833a52d69f7459b8e1cd42421b229
yeowool1010 <qyoong3579@gmail.com>
2023-01-29 16:18:57 +0900

    ✨ [feat] 셀렉트바 엔터 포커스이동 이벤트 추가 #2

--- old_shopping_mall/src/components/SearchBox.js
+++ old_shopping_mall/src/components/SearchBox.js
@@ -6,51 +6,62 @@ import { Category } from "../categoryDummy/CategoryDummy";
 
 /**
  * @author yeowool
- * @description 상품 카테고리(category)와 검색 키워드(searchInput) ListBox로 전달
+ * @description 상품 카테고리(category)와 검색 키워드(searchKeyword) 전역으로 관리하여 다른 컴포넌트에서 사용 가능하도록 함
  **/
 
 function SearchBox() {
   const dispatch = useDispatch();
 
   const search = useSelector((state) => state.search.value);
-  const getCategorie = search.category;
-  const getSearchInput = search.searchInput;
+  const getCategorys = search.category;
+  const getSearchInput = search.searchKeyword;
 
   const [selectedCtegory, setSelectedCtegory] = useState(Category[0]);
-  const [searchInput, setSearchInput] = useState(getSearchInput);
+  const [searchKeyword, setSearchInput] = useState(getSearchInput);
 
   const onSelectCategory = (e) => {
-    e.preventDefault();
     setSelectedCtegory(e.target.value);
   };
 
-  const onChangeSearchInput = (e) => {
-    e.preventDefault();
+  const onChangeSearchKeyword = (e) => {
     setSearchInput(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(Search({ category: selectedCtegory, searchInput: searchInput }));
+    dispatch(
+      Search({ category: selectedCtegory, searchKeyword: searchKeyword })
+    );
+  };
+
+  const moveFocus = (e) => {
+    if (e.keyCode === 13) {
+      document.getElementById("search-input").focus();
+    }
   };
 
   return (
     <section id="search-box">
       <h2>상품 검색</h2>
       <form onSubmit={handleSubmit} className="search-inner">
-        <select id="select-box" onChange={onSelectCategory}>
+        <select
+          id="select-box"
+          onChange={onSelectCategory}
+          onKeyDown={moveFocus}
+        >
           {Category.map((menu, idx) => (
-            <option key={idx} value={menu} selected={getCategorie === menu}>
+            <option key={idx} value={menu} selected={getCategorys === menu}>
               {menu}
             </option>
           ))}
         </select>
         <input
-          onChange={onChangeSearchInput}
+          id="search-input"
+          onChange={onChangeSearchKeyword}
           className="input-box"
           type="text"
           placeholder="입력하세요."
-          value={searchInput}
+          value={searchKeyword}
         ></input>
         <button type="submit" className="search-btn">
           조회
